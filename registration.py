4ab42d828dd640d64cfd3d5141ce3d508a682a13
casezumbrum <casezumbrum@ufl.edu>
2025-01-22 10:51:29 -0500

    update comments and names

--- registration.py
+++ registration.py
@@ -1,52 +1,52 @@
 import cv2
 import numpy as np
 
-img1_color = cv2.imread("prop_images/cropped.png")  # Image to be aligned.
-img2_color = cv2.imread("prop_images/ref.png")    # Reference image.
+subject_img = cv2.imread("prop_images/cropped.png")  # Image to be aligned.
+ref_img = cv2.imread("prop_images/ref.png")    # Reference image.
 
-# Convert to grayscale.
-img1 = cv2.cvtColor(img1_color, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2_color, cv2.COLOR_BGR2GRAY)
-height, width = img2.shape
+# Greyscale images
+subject_gray = cv2.cvtColor(subject_img, cv2.COLOR_BGR2GRAY)
+ref_gray = cv2.cvtColor(ref_img, cv2.COLOR_BGR2GRAY)
+height, width = ref_gray.shape
 
-# Create ORB detector with 5000 features.
+# Orbs are used for point detection
 orb_detector = cv2.ORB_create(5000)
 
-# Find keypoints and descriptors.
-# The first arg is the image, second arg is the mask
-#  (which is not required in this case).
-kp1, d1 = orb_detector.detectAndCompute(img1, None)
-kp2, d2 = orb_detector.detectAndCompute(img2, None)
+# get key points and descriptors from both images
+key_points_subject, descriptors_subject = orb_detector.detectAndCompute(subject_gray, None)
+key_points_ref, descriptors_ref = orb_detector.detectAndCompute(ref_gray, None)
 
-# Match features between the two images.
-# We create a Brute Force matcher with 
-# Hamming distance as measurement mode.
+# matcher object used by opencv to find minimum distance orientation between the two sets of descriptors
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 
-# Match the two sets of descriptors.
-matches = matcher.match(d1, d2)
+# perform matching
+matches = matcher.match(descriptors_subject, descriptors_ref)
 
-# Sort matches on the basis of their Hamming distance.
+# Distance here is the avg distance between desciptors in the match
+# get the lowest distance (best match)
 matches = sorted(matches, key = lambda x: x.distance)
 
-# Take the top 90 % matches forward.
+# Take the top 90 % matches
 matches = matches[:int(len(matches)*0.9)]
 no_of_matches = len(matches)
+
+
+
 
 # Define empty matrices of shape no_of_matches * 2.
 p1 = np.zeros((no_of_matches, 2))
 p2 = np.zeros((no_of_matches, 2))
 
 for i in range(len(matches)):
-  p1[i, :] = kp1[matches[i].queryIdx].pt
-  p2[i, :] = kp2[matches[i].trainIdx].pt
+  p1[i, :] = key_points_subject[matches[i].queryIdx].pt
+  p2[i, :] = key_points_ref[matches[i].trainIdx].pt
 
 # Find the homography matrix.
 homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
 
 # Use this matrix to transform the
 # colored image wrt the reference image.
-transformed_img = cv2.warpPerspective(img1_color,
+transformed_img = cv2.warpPerspective(subject_img,
                     homography, (width, height))
 
 # Save the output.
